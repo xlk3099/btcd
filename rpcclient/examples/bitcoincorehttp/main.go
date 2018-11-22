01b8a86be01cd0b7a550949e8546e59cca530e8b
xlk3099 <xlk3099@gmail.com>
2018-11-22 10:35:08 +0800

    change get block rpc: use verbosity

--- rpcclient/examples/bitcoincorehttp/main.go
+++ rpcclient/examples/bitcoincorehttp/main.go
@@ -7,15 +7,16 @@ package main
 import (
 	"log"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
 )
 
 func main() {
 	// Connect to local bitcoin core RPC server using HTTP POST mode.
 	connCfg := &rpcclient.ConnConfig{
-		Host:         "localhost:8332",
-		User:         "yourrpcuser",
-		Pass:         "yourrpcpass",
+		Host:         "localhost:9332",
+		User:         "lekai",
+		Pass:         "btccom",
 		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
 		DisableTLS:   true, // Bitcoin core does not provide TLS by default
 	}
@@ -28,9 +29,10 @@ func main() {
 	defer client.Shutdown()
 
 	// Get the current block count.
-	blockCount, err := client.GetBlockCount()
+	blockHash, _ := chainhash.NewHashFromStr("e8f29c11145bab2729fe4ea3cf2b44a64adef6815b8fb15c4795ae6b331eeed2")
+	rest, err := client.GetBlockVerboseTx(blockHash)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Block count: %d", blockCount)
+	log.Printf("Block count: %v", rest)
 }
